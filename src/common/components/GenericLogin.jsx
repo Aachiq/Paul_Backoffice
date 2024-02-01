6b47b67b2f40b68f0814988d8b50159d5b323b8d
Think Dev <=>
2024-02-01 13:00:10 +0100

    initialize bo paul

---
+++ src/common/components/GenericLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function GenericLogin(props) {
+    const { 
+        imgSrc,
+        mainTitle,
+        subTitle,
+        children,
+        buttonText,
+        isForgetPassword
+     } = props;
+  return (
+    <div>GenericLogin</div>
+  )
+}
