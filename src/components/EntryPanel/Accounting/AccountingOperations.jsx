cb340b6dbdd13ef6a44fd21fa451c54531cabe78
Think Dev <=>
2024-02-19 13:19:04 +0100

    add select & search input & logic status colors & params sidepar wip

--- src/components/EntryPanel/Accounting/AccountingOperations.jsx
+++ src/components/EntryPanel/Accounting/AccountingOperations.jsx
@@ -1,5 +1,6 @@
 import { Box, Chip, FormControl, InputLabel, MenuItem, Paper, Select, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material'
 import React, { useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function AccountingOperations() {
 
@@ -21,7 +22,7 @@ export default function AccountingOperations() {
         porboire:25.00,
         SousTotal:100,
         total:200,
-        status:"Echoué",
+        status:"Échoué",
         statusPos:"Notifié",
     },
     {
@@ -31,7 +32,7 @@ export default function AccountingOperations() {
         porboire:25.00,
         SousTotal:100,
         total:200,
-        status:"Echoué",
+        status:"Échoué",
         statusPos:"Non Notifié",
     },
     {
@@ -51,7 +52,7 @@ export default function AccountingOperations() {
         porboire:25.00,
         SousTotal:100,
         total:200,
-        status:"Echoué",
+        status:"Échoué",
         statusPos:"Notifié",
     },
     {
@@ -107,39 +108,79 @@ export default function AccountingOperations() {
   ]
   const paginationItems = [1,2,3,4,5];
   const [activedItemPagination, setActivedItemPagination] = useState(1);
+  // select logic
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const getStatusBackgorundColor = (status) => {
+        let statusColor = "";
+        switch(status){
+            case "Réussi": 
+                statusColor = "#90F487";
+                break; 
+            case "Échoué": 
+                statusColor = "#FF7B7B";
+                break; 
+            case "Remboursement partiel ": 
+                statusColor = "#E787FF";
+                break; 
+            case "Remboursement intégral ": 
+                statusColor = "#75F7FF";
+                break; 
+            default:
+                break ;
+        }
+        return statusColor;
+    }
   return (
     <Box 
         sx={{
             backgroundColor:"white",
             border:"solid #E5E5E5 0.5px",
             borderRadius: "10px",
-            padding:"20px"
+            padding:"18px"
         }}
     >
         <Box sx={{display:"flex", justifyContent:"space-between"}}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <img src="/images/search_icon.svg" alt="searhc_icon" />
-                <TextField id="input-with-sx" label="search by date" variant="standard" />
+                {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                <SearchIcon fontSize="20px" />
+                <TextField id="input-with-sx" variant="standard" placeholder='Search' 
+                    sx={{marginLeft:"5px"}}
+                />
             </Box>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <img src="/images/search_icon.svg" alt="searhc_icon" />
-                <TextField id="input-with-sx" label="search by amount" variant="standard" />
+                {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                <SearchIcon fontSize="20px" />
+                <TextField id="input-with-sx" variant="standard" placeholder='Search' 
+                    sx={{marginLeft:"5px"}}
+                />
             </Box>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <img src="/images/search_icon.svg" alt="searhc_icon" />
-                <TextField id="input-with-sx" label="search by cart" variant="standard" />
+                {/* <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} /> */}
+                <SearchIcon fontSize="20px" />
+                <TextField id="input-with-sx" variant="standard" placeholder='Search' 
+                    sx={{marginLeft:"5px"}}
+                />
             </Box>
+            
             <Box>
-                <FormControl sx={{width:"200px"}}>
-                    <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                    <InputLabel id="demo-select-small-label">Status</InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        value={age}
                         label="Age"
-                        // value={age}
-                        // onChange={handleChange}
+                        onChange={handleChange}
                     >
-                        <MenuItem value={10} selected>Tous</MenuItem>
+                        {/* <MenuItem value="" selected>
+                            <em>None</em>
+                        </MenuItem> */}
+                        <MenuItem value={10}>Tous</MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
                         <MenuItem value={30}>Thirty</MenuItem>
                     </Select>
@@ -148,18 +189,18 @@ export default function AccountingOperations() {
         </Box>
         {/*end header tab & start table */}
         <TableContainer sx={{marginTop:"20px"}}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <Table sx={{ minWidth: 700 }} aria-label="simple table">
                 <TableHead sx={{backgroundColor: '#F0F0F0'}}>
                     <TableRow>
-                        <TableCell sx={{color:"#7C7C7C"}}>Table</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Date</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Méthode de paiment</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Pourboire</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Sous-total</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Total</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Status</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Status-Pos</TableCell>
-                        <TableCell sx={{color:"#7C7C7C"}} align="right">Actions</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500}}>Table</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Date</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Méthode de paiment</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Pourboire</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Sous-total</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Total</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Status</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Status-Pos</TableCell>
+                        <TableCell sx={{color:"#7C7C7C", fontSize:"12px", fontWeight:500, padding:"8px"}} align="left" >Actions</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -173,27 +214,38 @@ export default function AccountingOperations() {
                     </TableCell>
                         <TableCell align="right">{row.date}</TableCell>
                         <TableCell align="right">{row.paymentMethod}</TableCell>
-                        <TableCell align="right">{row.porboire}</TableCell>
-                        <TableCell align="right">{row.SousTotal}</TableCell>
-                        <TableCell align="right">{row.total}</TableCell>
+                        <TableCell align="right">{row.porboire + "Dh"}</TableCell>
+                        <TableCell align="right">{row.SousTotal + "Dh"}</TableCell>
+                        <TableCell align="right">{row.total + "Dh"}</TableCell>
                         <TableCell align="right">
                             <Stack direction="row" spacing={1}>
                                 <Chip label={row.status} sx={{
-                                    backgroundColor: row.status === "Réussi" ? "#90F487" :
-                                                        row.status === "Echoué" ? "#FF7B7B" :
-                                                        row.status === "Remboursement partiel" ? "#E787FF" :
-                                                        row.status === "Remboursement intégrale" ? "#75F7FF" : null
+                                        backgroundColor: getStatusBackgorundColor(row.status)
                                     }} 
                                 />
+                                { row.status === "Échoué" && <img src="/images/accounting/icon-failed-status.svg" alt="icon-failed-status" /> }
                             </Stack> 
                         </TableCell>
                         <TableCell align="right">
                             <Stack direction="row" spacing={1}>
                                 <Chip label={row.statusPos} sx={{
-                                    backgroundColor: row.statusPos === "Notifié" ? "#90F487" : "#FDFF9B"
+                                        backgroundColor: row.statusPos === "Notifié" ? "#90F487" : "#FDFF9B"
                                     }} 
                                 />
                             </Stack> 
+                        </TableCell>
+                        <TableCell align="right">
+                            <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                                <img src="/images/accounting/action-icon-1.svg" alt="action-icon-1" 
+                                    //  style={{marginX:"5px"}}
+                                />
+                                <img src="/images/accounting/action-icon-2.svg" alt="action-icon-2" 
+                                     style={{marginLeft:"10px", marginRight:"10px"}}
+                                />
+                                <img src="/images/accounting/action-icon-3.svg" alt="action-icon-3" 
+                                    //  style={{marginX:"5px"}}
+                                />
+                            </Box>
                         </TableCell>
                     </TableRow>
                 ))}
@@ -224,6 +276,7 @@ export default function AccountingOperations() {
                                 alignItems:"center",
                                 background: item === activedItemPagination ? "linear-gradient(to right, #8419E2, #F13727)" : "none",
                                 color: item === activedItemPagination ? "white" : "black",
+                                margin:"3px"
                             }}
                         >
                           {item}
